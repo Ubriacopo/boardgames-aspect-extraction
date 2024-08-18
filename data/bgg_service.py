7c203ec9e77455c2c96cefe1757e6b810a7943ad
jacopo <jacopo.fichera98@gmail.com>
2024-08-18 16:25:52 +0200

    Updated service (Should be working)

--- data/bgg_service.py
+++ data/bgg_service.py
@@ -1,3 +1,11 @@
+import requests
+
+import pandas as pd
+from pandas import DataFrame, Series
+
+from xmljson import yahoo
+from xml.etree.ElementTree import fromstring
+
 """
     Service that loads the reviews from BGG for a given game. (We use BGG-APIv2)
 
@@ -13,16 +21,40 @@
         and review on BGG and so I'd be creating a low bias towards those games?
 """
 
+BGG_URL = "https://boardgamegeek.com/xmlapi2/thing?id={id}&stats=1&comments=1&page={page}"
+
 
 class BggService:
+    games_dataframe: DataFrame = None
+    latest_game: Series = None
+
     def __init__(self, endpoint: str, game_list_csv_path: str = "./resources/2024-08-18.csv"):
         # https://boardgamegeek.com/xmlapi2/thing?id={csv-id}&stats=1&comments=1&page=3
         self.endpoint: str = endpoint
 
         # Load csv data with Pandas.
+        self.games_dataframe = pd.read_csv(game_list_csv_path)
+        self.current_game_index = 0
+        self.current_game_meta_page = 1
 
     def load_next(self):
         """
         Returns comments of the next boardgame in list from the loaded csv. (State has to be persisted)
         """
-        pass
+        self.latest_game = self.games_dataframe.loc[self.current_game_index]
+        self.current_game_meta_page = 1
+
+        response = requests.get(BGG_URL.format(id=self.latest_game["ID"], page=self.current_game_meta_page))
+        return yahoo.data(fromstring(response.content))
+
+    def load_more(self):
+        """
+        Goes to next page of selected game metadata (Comments, etc)
+        """
+        if self.latest_game is None:
+            raise Exception("No more information for the current game is available")
+
+        self.current_game_meta_page += 1
+
+        response = requests.get(BGG_URL.format(id=self.latest_game["ID"], page=self.current_game_meta_page))
+        return yahoo.data(fromstring(response.content))
