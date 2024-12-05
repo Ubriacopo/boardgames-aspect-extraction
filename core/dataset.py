0642beffe9f681c2191093bb19bc871f5700c7da
jacopo <jacopo.fichera98@gmail.com>
2024-12-05 18:37:42 +0100

    WIP

--- core/dataset.py
+++ core/dataset.py
@@ -1,9 +1,12 @@
+import gensim
 import numpy as np
 import pandas as pd
 import spacy
 from keras import preprocessing as pre
 from torch.utils.data import Dataset
 import swifter
+
+from core.embeddings import WordEmbedding
 
 
 class PositiveNegativeCommentGeneratorDataset(Dataset):
@@ -67,3 +70,50 @@ class PositiveNegativeCommentGeneratorDataset(Dataset):
 
     def __len__(self):
         return len(self.dataset) - 1
+
+
+class EmbeddingsDataset(Dataset):
+
+    def generate_numeric_representation(self, entry):
+        # Map each word to the correct representation. If it does not exist <UNK> value is returned.
+        return np.array([self.vocabulary[t] if t in self.vocabulary else self.vocabulary['<UNK>'] for t in entry])
+
+    def __init__(self, csv_dataset_path: str, embeddings_model: gensim.models.Word2Vec, max_seq_length: int = 256):
+        super(EmbeddingsDataset).__init__()
+        self.embeddings_model = embeddings_model
+        self.nlp = spacy.blank("en")
+
+        self.vocabulary = self.embeddings_model.wv.key_to_index
+
+        print(f"Loading dataset from file: {csv_dataset_path}")
+        self.dataset = pd.read_csv(csv_dataset_path).dropna()
+
+        print("Generating numeric representation for each word of ds.")
+
+        self.text_ds = self.dataset["original_text"]
+        self.id_ds = self.dataset["game_id"]
+
+        self.dataset = self.dataset["comments"].swifter.apply(
+            lambda x: self.generate_numeric_representation([token.text for token in self.nlp(x)])
+        )
+        print("Max sequence length calculation in progress...")
+        # optimal_length = int(np.percentile(lengths, 95))  # 95th percentile. 512 is above that. (525/169973) ~ 0.3 %
+        max_found_length = self.dataset.map(lambda x: len(x)).max()
+        with_lost_information = self.dataset.map(lambda x: len(x) > max_seq_length).sum()
+        print(f"We loose information on {with_lost_information} points."
+              f"This is {with_lost_information / len(self.dataset) * 100}% of the dataset.")
+
+        print(f"Padding sequences to max length ({max_seq_length}).")
+        self.dataset = pd.Series(pre.sequence.pad_sequences(self.dataset, maxlen=max_seq_length).tolist())
+
+    def __getitem__(self, index):
+        return np.array(self.dataset.at[index])
+        # vectors = [self.embeddings_model.wv[word] for word in sentence if word in self.embeddings_model.wv]
+        # vectors = np.mean(vectors, axis=0) if len(vectors) > 0 else np.zeros(self.embeddings_model.vector_size)
+        # return self.embeddings_model()
+
+    def __len__(self):
+        return len(self.dataset) - 1
+
+    def get_full_dataset(self):
+        return np.array([np.array(e) for e in self.dataset])
--- core/evaluation.py
+++ core/evaluation.py
@@ -15,21 +15,18 @@ def extract_top_k_words_of_aspect(aspect: torch.tensor, word_embeddings,
                                   inverse_vocabulary: dict, top_k: int = 25, verbose: bool = True) -> list:
     """
     This function will extract the top k words of each aspect.
-    @return: A list of aspects with their top k words.
+    @return: A list of aspects with their top k words. [str, float, int]
     """
-    # We will extract the top k words of each aspect
-    top_k_words = []
-
     aspect = aspect.cpu()
     similarity = word_embeddings.matmul(aspect).detach().numpy()
 
     ordered_words = np.argsort(similarity)[::-1]
-    desc_list = [(inverse_vocabulary[w], similarity[w]) for w in ordered_words[:top_k]]
+    top_k_words = [(inverse_vocabulary[w], similarity[w], w) for w in ordered_words[:top_k]]
     # Normalize the data for comparison
 
     if verbose:
         print("\nGiven aspect most representative words are:")
-        for i in desc_list:
+        for i in top_k_words:
             # hr][/i is not a valid word. meh. todo: Process better the words.
             print("Word: ", i[0], f"({i[1]})")
 
@@ -38,15 +35,32 @@ def extract_top_k_words_of_aspect(aspect: torch.tensor, word_embeddings,
 
 def coherence(top_n_words: list, corpus: PositiveNegativeCommentGeneratorDataset):
     coherence_score = 0
-    itertools.combinations(top_n_words, 2)
+
+    document_frequency_values = dict()
+    for w in top_n_words:
+        document_frequency_values[w] = document_frequency(w, corpus)
+
+    # Exponential complexity. We can optimize this.
+    document_co_frequency_values = dict()
     for w_i, w_j in itertools.combinations(top_n_words, 2):
-        coherence_score += np.log((document_co_occurrence(w_i, w_j, corpus) + 1) / document_frequency(w_j, corpus))
-    return coherence_score
+        co_occurrence = document_co_occurrence(w_i, w_j, corpus)
+        document_co_frequency_values[(w_i, w_j)] = co_occurrence
+        document_co_frequency_values[(w_j, w_i)] = co_occurrence
+
+    for i in range(2, len(top_n_words)):
+        w_i = top_n_words[i]
+
+        for j in range(i - 1):
+            w_j = top_n_words[j]
+
+            coherence_score += np.log((document_co_frequency_values[(w_i, w_j)] + 1) / document_frequency_values[w_j])
+
+    return coherence_score / len(top_n_words)
 
 
 def document_frequency(a: str | int, corpus: PositiveNegativeCommentGeneratorDataset) -> float:
-    return corpus.dataset.apply(lambda x: a in x).value_counts()[1]
+    return corpus.dataset.apply(lambda x: a in x).value_counts().get(True, 0)
 
 
 def document_co_occurrence(a: str | int, b: str | int, corpus: PositiveNegativeCommentGeneratorDataset) -> float:
-    return corpus.dataset.apply(lambda x: a in x and b in x).value_counts()[1]
+    return corpus.dataset.apply(lambda x: a in x and b in x).value_counts().get(True, 0)
